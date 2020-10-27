df8b5156b815a077b8b9be102262757a3317105c
Julian Tovar <julian.tovar360@gmail.com>
2020-10-26 18:53:48 -0600

    Initial commit 2

---
+++ NearestNeighborAlgorithm/src/com/tso/jatd/Main.java
@@ -0,0 +1,12 @@
+package com.tso.jatd;
+
+import com.tso.jatd.algorithms.NNAlgorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NNAlgorithm algorithm = new NNAlgorithm();
+        algorithm.solve();
+        System.out.println("Hola mundo");
+    }
+}
---
+++ NearestNeighborAlgorithm/src/com/tso/jatd/Node.java
@@ -0,0 +1,42 @@
+package com.tso.jatd;
+
+public class Node {
+
+    private String name;
+    private Integer positionX;
+    private Integer positionY;
+
+    public Node() {
+
+    }
+
+    public Node(String name, Integer positionX, Integer positionY) {
+        this.name = name;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public Integer getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(Integer positionX) {
+        this.positionX = positionX;
+    }
+
+    public Integer getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(Integer positionY) {
+        this.positionY = positionY;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
